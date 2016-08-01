a385a7c8f6e957fda175d8af9546e68f889753a4
Roberto Barros <roberto.f.barros@gmail.com>
2016-08-01 10:43:28 -0300

    aula de middleman

--- config.rb
+++ config.rb
@@ -31,6 +31,10 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+['mathieu','thiago'].each do |owner|
+  proxy "/flats/#{owner}.html", '/flats/show.html', locals: { owner: owner}, ignore: true
+end
+
 ###
 # Helpers
 ###
